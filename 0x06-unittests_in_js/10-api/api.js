9dfcd40f8b9203abad4d153ba04e3e5e6e35b0f0
Robert Kimaiyo <97959136+robert5313@users.noreply.github.com>
2023-09-23 00:07:26 +0300

    Update api.js

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,20 +3,23 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
-app.get('/', (req, res) => res.end('Welcome to the payment system'))
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.end(`Payment methods for cart ${req.params.id}`)
-})
+app.use(express.json());
+
 app.get('/available_payments', (req, res) => {
-  const obj = {
-    payment_methods: {
-      credit_cards: true,
-      paypal: false,
-    },
+  const paymentMethods = {
+    credit_cards: true,
+    paypal: false
   };
-  res.json(obj);
+  res.json({ payment_methods: paymentMethods });
 });
+
 app.post('/login', (req, res) => {
-  res.end(`Welcome ${req.body.userName}`);
+  const { userName } = req.body;
+  res.send(`Welcome ${userName}`);
 });
-app.listen(port, () => console.log(`API available on localhost port ${port}`));
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
