c514602bcc048760f9a8e7f6f7f5540fbb1319c4
robert5313 <robert.kimaiyo@masterclass.co>
2023-11-23 23:05:58 +0000

    utils updates

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,10 @@
+const calculateNumber = (type, a, b) => {
+    if (type === 'SUM') {
+      return a + b;
+    }
+    // handle other types here
+  };
+  
+  module.exports = {
+    calculateNumber,
+  };
